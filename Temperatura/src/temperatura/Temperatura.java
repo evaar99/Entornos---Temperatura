c79a7061cf37dd985dae7f585d3e2e777bb47672
evaren <evaren@ubuntu>
2021-12-02 17:39:18 +0100

    He creado un programa que da la bienvenida.

---
+++ Temperatura/src/temperatura/Temperatura.java
@@ -0,0 +1,7 @@
+package temperatura;
+public class Temperatura {
+    public static void main(String[] args) {
+        System.out.println("¡Bienvenido al programa de las temperaturas!");        
+    }
+    
+}
