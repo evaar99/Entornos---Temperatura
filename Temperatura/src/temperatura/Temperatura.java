dc81166a14858029d7136d5d4b582603a9a33a38
evaren <evaren@ubuntu>
2021-12-02 18:23:26 +0100

    He creado un programa que da la bienvenida. 1. He añadido que el usuario introduzca la temperatura y el programa te responde si la temperatura introducida es positiva o negativa. 2. He añadido que el usuario introduzca 5 temperaturas e indicará en cada caso si es positiva o negativa.

--- Temperatura/src/temperatura/Temperatura.java
+++ Temperatura/src/temperatura/Temperatura.java
@@ -9,13 +9,16 @@ public class Temperatura {
         double temp;
         Scanner leer = new Scanner(System.in);
         System.out.println("¡Bienvenido al programa de las temperaturas!");
-        System.out.println("Por favor, introduzca la temperatura.");
-        temp=leer.nextDouble();
-        
-        if (temp>=0) {
-            System.out.println("La temperatura es positiva.");
-        } else {
-            System.out.println("La temperatura es negativa·");
+
+        for (int cont = 1; cont <= 5; cont++) {
+            System.out.println("Por favor, introduzca la temperatura.");
+            temp = leer.nextDouble();
+
+            if (temp >= 0) {
+                System.out.println("La temperatura es positiva.");
+            } else {
+                System.out.println("La temperatura es negativa.");
+            }
         }
     }
 
