fd2377f1942aba2dd1d6959478520f496cf40202
evaren <evaren@ubuntu>
2021-12-09 17:22:19 +0100

    El programa ya muestra cual es la temperatura menor y la mayor de un número X de temperaturas introducidas por teclado.

--- Temperatura/src/temperatura/Temperatura.java
+++ Temperatura/src/temperatura/Temperatura.java
@@ -7,7 +7,7 @@ public class Temperatura {
     public static void main(String[] args) {
 
         //Variable que nos va a guardar las temperaturas introducidas.
-        double temp1, temp2, tempmen, tempmay;
+        double temp , tempmen=1000, tempmay=-1000;
         //Variable que indica la cantidad que quiere introducir.
         int cantidad;
         Scanner leer = new Scanner(System.in);
@@ -15,17 +15,15 @@ public class Temperatura {
         System.out.println("¿Cuántas temperaturas desea introducir?");
         cantidad = leer.nextInt();
 
-        System.out.println("Por favor, introduzca la temperatura.");
-        temp1 = leer.nextDouble();
-        for (int cont = 1; cont < cantidad; cont++) {
+        //System.out.println("Por favor, introduzca la temperatura.");
+        //temp1 = leer.nextDouble();
+        for (int cont = 1; cont <= cantidad; cont++) {
             System.out.println("Por favor, introduzca la temperatura.");
-            System.out.println("La temperatura de referencia es " + temp1);
-            temp2 = leer.nextDouble();
-            if (temp2 > tempmay) {
-                tempmay = temp2;
-            }
-            if (temp2 < tempmen) {
-                tempmen = temp2;
+            temp = leer.nextDouble();
+            if (temp > tempmay) {
+                tempmay = temp;
+            } else if (temp < tempmen) {
+                tempmen = temp;
             }
 
         }
