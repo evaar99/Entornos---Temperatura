7049ecf4231436aade38314c9ea76653cf3f1422
evaren <evaren@ubuntu>
2021-12-16 18:51:24 +0100

    +1 variable (municipio). Muestra municipio junto temperatura.

--- Temperatura/src/temperatura/Temperatura.java
+++ Temperatura/src/temperatura/Temperatura.java
@@ -7,21 +7,27 @@ public class Temperatura {
     public static void main(String[] args) {
 
         //Variable que nos va a guardar las temperaturas introducidas.
-        double temp[], tempmay = Double.MIN_VALUE, tempmen =Double.MAX_VALUE;
+        double temp[], tempmay = Double.MIN_VALUE, tempmen = Double.MAX_VALUE;
+        //Array que guarda el nombre de los municipios.
+        String municipio[];
         //Variable que indica la cantidad que quiere introducir.
         int cantidad;
-        
+
         Scanner leer = new Scanner(System.in);
         System.out.println("¡Bienvenido al programa de las temperaturas!");
         System.out.println("¿Cuántas temperaturas desea introducir?");
         cantidad = leer.nextInt();
-        temp=new double[cantidad];
+        temp = new double[cantidad];
+        municipio = new String[cantidad];
 
         //System.out.println("Por favor, introduzca la temperatura.");
         //temp1 = leer.nextDouble();
         for (int cont = 0; cont < cantidad; cont++) {
-            System.out.println("Por favor, introduzca la temperatura.");
+            System.out.print("Por favor, introduzca la temperatura.");
             temp[cont] = leer.nextDouble();
+            System.out.print("¿Cómo se llama el municipio?");
+            municipio[cont] = leer.next();
+
             //Indicaremos si la temperatura es positiva o negativa
             if (temp[cont] < 0) {
                 System.out.println("La temperatura esta en negativo");
@@ -30,26 +36,25 @@ public class Temperatura {
                 System.out.println("La temperatura esta en positivo");
 
             }
-            if(cont==1){
+            if (cont == 1) {
                 tempmay = temp[cont];
                 tempmen = temp[cont];
-                
-            }
-            else{
+
+            } else {
                 if (temp[cont] > tempmay) {
-                tempmay = temp[cont];
+                    tempmay = temp[cont];
+                }
+                if (temp[cont] < tempmen) {
+                    tempmen = temp[cont];
+                }
             }
-            if (temp[cont] < tempmen) {
-                tempmen = temp[cont];
-            }
-            }
-            
 
         }
         System.out.println("La temperatura mayor es " + tempmay + " y la menor es " + tempmen);
-        System.out.println("Las temperaturas introducidas son: ");
-        for (int i=0; i< temp.length;i++){
-            System.out.println((temp[i]));
+        System.out.println("Las temperaturas y municipios introducidos son: ");
+        for (int i = 0; i < temp.length; i++) {
+            System.out.println((municipio[i]) + " : " + (temp[i]));
         }
+        
     }
 }
